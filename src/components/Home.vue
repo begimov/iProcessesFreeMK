c89cc8551d6298852222916c6fae737e701e804f
begimov <begimov@gmail.com>
2018-01-15 14:37:23 +0300

    top fluid container for a header created

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links2</h2>
+  <div>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Бизнес как система</h1>
+        <p class="lead">практический онлайн-марафон</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,18 +22,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
