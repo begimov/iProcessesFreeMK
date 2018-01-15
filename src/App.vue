51f5324ff05fc8ccf6450a892933a2536dfdd43d
begimov <begimov@gmail.com>
2018-01-15 15:41:35 +0300

    footer added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,40 @@
 <template>
   <div id="app">
     <router-view/>
-    Footer
+    <div class="container-fluid border border-right-0 border-left-0 border-bottom-0 border-light">
+      <div class="row ifooter">
+        <div class="col-md-4">
+          <a href="https://iteam.ru/" target="_blank">
+            <img src="https://iteam.ru/landing/template/img/logo.png">
+          </a><br><br>
+          <p>&copy;&nbsp;2002&nbsp;&mdash; 2017 iTeam<br>Москва, Пресненская наб.&nbsp;12</p>
+        </div>
+        <div class="col-md-4">
+          <ul class="list-inline">
+            <li class="list-inline-item">
+              <a href="https://www.facebook.com/ITeam-Management-1538687959484919/">
+                <img src="https://iteam.ru/landing/template/img/facebook.png">
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://vk.com/public150574411">
+                <img src="https://iteam.ru/landing/template/img/vk.png">
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.instagram.com/iteamanagement/">
+                <img src="https://iteam.ru/landing/template/img/instagram.png">
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <a href="https://iteam.ru/company/terms">Пользовательское соглашение</a>
+          <br>
+          <a href="https://iteam.ru/company/offer">Реквизиты</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,3 +43,9 @@ export default {
   name: 'App'
 }
 </script>
+
+<style scoped>
+.ifooter {
+  padding: 20px;
+}
+</style>
