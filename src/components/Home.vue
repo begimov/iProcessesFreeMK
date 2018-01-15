d1020c5c870494713772ba79dacf4143b73421ce
begimov <begimov@gmail.com>
2018-01-15 15:02:36 +0300

    tabs block added to Home comonent layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -41,6 +41,27 @@
         </div>
       </div>
 
+      <div class="row">
+        <div class="col">
+          <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item">
+              <a class="nav-link active" id="first-tab" data-toggle="tab" href="#first" role="tab" aria-controls="first" aria-selected="true">12 февраля</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="second-tab" data-toggle="tab" href="#second" role="tab" aria-controls="second" aria-selected="false">24 февраля</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="third-tab" data-toggle="tab" href="#third" role="tab" aria-controls="conthirdtact" aria-selected="false">5 марта</a>
+            </li>
+          </ul>
+          <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="first" role="tabpanel" aria-labelledby="first-tab">1</div>
+            <div class="tab-pane fade" id="second" role="tabpanel" aria-labelledby="second-tab">2</div>
+            <div class="tab-pane fade" id="third" role="tabpanel" aria-labelledby="third-tab">3</div>
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
