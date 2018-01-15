5b78b08ef2401ce14741db7c1c63d933171c3bd6
begimov <begimov@gmail.com>
2018-01-15 14:24:10 +0300

    About component created and added to vue-router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import About from '@/components/about/About'
 
 Vue.use(Router)
 
@@ -13,8 +14,8 @@ export default new Router({
     },
     {
       path: '/about',
-      name: 'Home',
-      component: Home
+      name: 'About',
+      component: About
     }
   ]
 })
