b9e7377440435fbf4321702e2af52d57bfe3e515
begimov <begimov@gmail.com>
2018-01-15 14:43:01 +0300

    basic layout block added to Home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,6 +6,19 @@
         <p class="lead">практический онлайн-марафон</p>
       </div>
     </div>
+    <div class="container">
+
+      <div class="row">
+        <div class="col">
+          <h2>Вам знакомы эти проблемы?</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <p>Компания малоуправляема...</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
